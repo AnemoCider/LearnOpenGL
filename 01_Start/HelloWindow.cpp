d0cf437ed7d3b07fd58f3a7c4ef5fcd2d0e1e5c0
AnemoCider <junwenyu@umich.edu>
2023-12-05 16:50:25 -0500

    Init Commit

---
+++ 01_Start/HelloWindow.cpp
@@ -0,0 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // When proper version of opengl is not found, set version to 3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile only
+    return 0;
+}
